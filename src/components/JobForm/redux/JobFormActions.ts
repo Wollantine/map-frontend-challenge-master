430f2d99ecb61dd2741d2d6a963308b851ae8ff3
Kwirke <nebrios2@gmail.com>
2018-06-23 20:04:52 +0200

    Add jobFormReducer tests

--- src/components/JobForm/redux/JobFormActions.ts
+++ src/components/JobForm/redux/JobFormActions.ts
@@ -1,7 +1,20 @@
-export const UPDATE_FIELD = 'UPDATE_FIELD';
+import { EFieldStatus } from "./field";
 
-export const updateField = (fieldName: string, value: any) => ({
+export const UPDATE_FIELD = 'UPDATE_FIELD';
+export const START_CREATING_JOB = 'START_CREATING_JOB';
+export const FINISH_CREATING_JOB = 'FINISH_CREATING_JOB';
+
+export const updateField = (fieldName: string, value: any, status: EFieldStatus) => ({
     type: UPDATE_FIELD,
     fieldName,
     value,
+    status,
+});
+
+export const startCreatingJob = () => ({
+    type: START_CREATING_JOB,
+});
+
+export const finishCreatingJob = () => ({
+    type: FINISH_CREATING_JOB,
 });
---
+++ src/components/JobForm/redux/tests/jobFormReducer.test.ts
@@ -0,0 +1,59 @@
+import {expect} from 'chai';
+import { fieldReducer, creating } from "../jobFormReducer";
+import { EFieldStatus } from '../field';
+import { updateField, startCreatingJob, finishCreatingJob } from '../JobFormActions';
+
+describe('jobFormReducer', () => {
+    describe('fieldReducer', () => {
+        it('should return a reducer', () => {
+            const sut = fieldReducer('test', '');
+            const state = sut(undefined, {type: 'UNDEFINED_ACTION'});
+            expect(sut(state, {type: 'UNDEFINED_ACTION'})).to.deep.equal(state);
+        });
+
+        describe('returned reducer', () => {
+            const name = 'testField';
+            const initialState = 'test';
+            const reducer = fieldReducer(name, initialState);
+
+            it('should return a TField', () => {
+                const state = reducer(undefined, {type: 'UNDEFINED_ACTION'});
+                expect(state).to.have.property('value').and.property('status');
+            });
+
+            it('should use initialState as default value and pristine as default status', () => {
+                const state = reducer(undefined, {type: 'UNDEFINED_ACTION'});
+                expect(state).to.deep.equal({value: initialState, status: EFieldStatus.pristine});
+            });
+
+            it('should update value and status on UPDATE_FIELD', () => {
+                const action = updateField(name, 'b', EFieldStatus.invalid);
+                const newState = reducer({value: 'a', status: EFieldStatus.pristine}, action);
+                expect(newState).to.deep.equal({value: 'b', status: EFieldStatus.invalid});
+            });
+
+            it('should not update anything if the fieldName is not the same', () => {
+                const action = updateField(name, 'b', EFieldStatus.invalid);
+                const state = {value: 'a', status: EFieldStatus.pristine};
+                expect(reducer(state, action)).to.deep.equal(state);
+            });
+        });
+    });
+
+    describe('creating', () => {
+        it('should be false by default', () => {
+            const newState = creating(undefined, {type: 'UNDEFINED_ACTION'});
+            expect(newState).to.be.false;
+        });
+
+        it('should set to true after START_CREATING_JOB', () => {
+            expect(creating(false, startCreatingJob())).to.be.true;
+            expect(creating(true, startCreatingJob())).to.be.true;
+        });
+
+        it('should set to false after FINISH_CREATING_JOB', () => {
+            expect(creating(false, finishCreatingJob())).to.be.false;
+            expect(creating(true, finishCreatingJob())).to.be.false;
+        });
+    });
+});
